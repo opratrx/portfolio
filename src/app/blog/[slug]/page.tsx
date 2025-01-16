9e9579570617f6a769bfa85687e2d15d1e6e1705
opratrx <im.opratr@gmail.com>
2025-01-15 18:43:52 -0800

    Fixed Type error: Type '{ params: { slug: string; }; }' does not satisfy the constraint 'PageProps'.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -52,12 +52,16 @@ export async function generateMetadata({
 }
 
 export default async function Blog({
-  params,
-}: {
-  params: {
-    slug: string;
+                                     params,
+                                   }: {
+  params?: {
+    slug?: string;
   };
 }) {
+  if (!params || !params.slug) {
+    notFound();
+  }
+
   let post = await getPost(params.slug);
 
   if (!post) {
@@ -65,43 +69,43 @@ export default async function Blog({
   }
 
   return (
-    <section id="blog">
-      <script
-        type="application/ld+json"
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "BlogPosting",
-            headline: post.metadata.title,
-            datePublished: post.metadata.publishedAt,
-            dateModified: post.metadata.publishedAt,
-            description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${DATA.url}${post.metadata.image}`
-              : `${DATA.url}/og?title=${post.metadata.title}`,
-            url: `${DATA.url}/blog/${post.slug}`,
-            author: {
-              "@type": "Person",
-              name: DATA.name,
-            },
-          }),
-        }}
-      />
-      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
-        {post.metadata.title}
-      </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
-        <Suspense fallback={<p className="h-5" />}>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">
-            {formatDate(post.metadata.publishedAt)}
-          </p>
-        </Suspense>
-      </div>
-      <article
-        className="prose dark:prose-invert"
-        dangerouslySetInnerHTML={{ __html: post.source }}
-      ></article>
-    </section>
+      <section id="blog">
+        <script
+            type="application/ld+json"
+            suppressHydrationWarning
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "BlogPosting",
+                headline: post.metadata.title,
+                datePublished: post.metadata.publishedAt,
+                dateModified: post.metadata.publishedAt,
+                description: post.metadata.summary,
+                image: post.metadata.image
+                    ? `${DATA.url}${post.metadata.image}`
+                    : `${DATA.url}/og?title=${post.metadata.title}`,
+                url: `${DATA.url}/blog/${post.slug}`,
+                author: {
+                  "@type": "Person",
+                  name: DATA.name,
+                },
+              }),
+            }}
+        />
+        <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
+          {post.metadata.title}
+        </h1>
+        <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+          <Suspense fallback={<p className="h-5" />}>
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+              {formatDate(post.metadata.publishedAt)}
+            </p>
+          </Suspense>
+        </div>
+        <article
+            className="prose dark:prose-invert"
+            dangerouslySetInnerHTML={{ __html: post.source }}
+        ></article>
+      </section>
   );
 }
